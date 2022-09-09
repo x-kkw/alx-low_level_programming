7e867f43a2a9cdc728d7c2f6185d59ff0f14d783
Kyalle Wamitila <xkkw@outlook.com>
2022-09-09 22:52:21 +0300

    16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry into the code
+ * Description: prints all numbers of base 16 starting from 0, followed by a new line.
+ * Return: 0
+ */
+
+
+int main(void)
+{
+	char c;
+	char d = '0';
+
+	while (d <= '9')
+	{
+		putchar(d);
+		d++;
+	}
+
+	for (c = 'a'; c <= 'f'; c++)
+	{
+		putchar(c);
+	}
+
+	putchar('\n');
+	return (0);
+}
