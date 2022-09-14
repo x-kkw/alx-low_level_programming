3210c736b8a42a4333ccf9977110da1cb89fb89a
Kyalle Wamitila <xkkw@outlook.com>
2022-09-15 00:45:31 +0300

    2,3

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+
+/**
+ * print_alphabet_x10 - prints the alphabet x10
+ *
+ * Return: no return
+ */
+
+
+void print_alphabet_x10(void)
+{
+	char c = '\0';
+	int n = 10;
+
+	while (n--)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+			_putchar(c);
+		_putchar('\n');
+	}
+}
