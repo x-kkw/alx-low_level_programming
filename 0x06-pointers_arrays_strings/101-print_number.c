f2b649f8a2b7f87854320e97775d4c7180f490f8
Kyalle Wamitila <xkkw@outlook.com>
2022-09-23 14:11:39 +0300

    bonus

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_number - Main function: prints an integer
+ * @n:.integer
+ *
+ *
+ */
+
+void print_number(int n)
+{
+	unsigned int i = n;
+
+	if (n < 0)
+	{
+		_putchar(45);
+		i = -i;
+	}
+	if (i / 10)
+	{
+		print_number(i / 10);
+	}
+	_putchar(i % 10 + '0');
+}
