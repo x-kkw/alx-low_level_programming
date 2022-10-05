a0430a25acf59737f57e4242fc3cc75987df05b6
Kyalle Wamitila <xkkw@outlook.com>
2022-10-05 21:27:02 +0300

    malloc

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * free_grid - use free function on previous grid
+ * @grid: 2 dimensional array to free
+ * @height: input height
+ * Return: 0
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	if (grid != NULL || height != 0)
+	{
+		for (i = 0; i < height; i++)
+		{
+			free(grid[i]);
+		}
+		free(grid);
+	}
+}
