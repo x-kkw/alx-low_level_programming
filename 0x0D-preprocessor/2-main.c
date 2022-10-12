f79e9e99bdfab6642f756313ca3b0a0991d032c8
Kyalle Wamitila <xkkw@outlook.com>
2022-10-12 21:51:06 +0300

    preprocessor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Prints the name of the file it was compiled from + new line
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
